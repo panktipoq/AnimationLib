2c582c894efa8b5b851314884a5f36a342e56773
Pankti Patel <pankti.patel@poqstudio.com>
2018-11-29 17:19:58 +0000

    improvement changes

--- Platform_AnimationDemo/PoqPlatform/Sources/Screens/AnimationLib/Core/AnimationKeyPaths.swift
+++ Platform_AnimationDemo/PoqPlatform/Sources/Screens/AnimationLib/Core/AnimationKeyPaths.swift
@@ -10,24 +10,6 @@ import Foundation
 import UIKit
 
 /*
- 
- AnimationKeyPaths is responsible to create custom AnimatorKeyPath
- NOTE: value type defined for AnimatorKeyPath should confirm to AnimationValueType
- */
-open class AnimationKeyPaths {
-    fileprivate init() {}
-}
-
-/*
- 
- AnimationValueType specifies the value type of the AnimatorKeyPath
- */
-
-public protocol AnimationValueType {}
-
-
-/*
- 
  AnimatorKeyPath returns the keypath of the animation
  NOTE: Value type defined for AnimatorKeyPath should confirm to AnimationValueType
  for e.g if Using backgroundColor as keyPath, CGColor should confirm to AnimationValueType
@@ -35,49 +17,38 @@ public protocol AnimationValueType {}
     ValueType : This is the type of the animation values, it can be used to restrict specific value type for keypath.
                 for e.g When backgroundColor keyPath is used, the from and to animation values has to be CGColor type only
  */
-public final class AnimatorKeyPath<ValueType: AnimationValueType> : AnimationKeyPaths {
+public struct AnimatorKeyPath {
     let rawValue: String
     init(keyPath: String) {
         rawValue = keyPath
     }
 }
 
-extension Array: AnimationValueType {}
-extension Dictionary: AnimationValueType {}
-extension Bool: AnimationValueType {}
-extension CGPoint: AnimationValueType {}
-extension CGSize: AnimationValueType {}
-extension CGColor: AnimationValueType {}
-extension CGFloat: AnimationValueType {}
-extension CATransform3D: AnimationValueType {}
-extension Int: AnimationValueType {}
-extension Double: AnimationValueType {}
-
-extension AnimationKeyPaths {
-    public static let opacity = AnimatorKeyPath<CGFloat>(keyPath: #keyPath(CALayer.opacity))
-    public static let radius = AnimatorKeyPath<CGFloat>(keyPath: #keyPath(CALayer.cornerRadius))
-    public static let position = AnimatorKeyPath<CGPoint>(keyPath: #keyPath(CALayer.position))
-    public static let backgroundColor = AnimatorKeyPath<CGColor>(keyPath: #keyPath(CALayer.backgroundColor))
-    public static let transform = AnimatorKeyPath<CATransform3D>(keyPath: #keyPath(CALayer.transform))
+extension AnimatorKeyPath {
+    public static let opacity = AnimatorKeyPath(keyPath: #keyPath(CALayer.opacity))
+    public static let radius = AnimatorKeyPath(keyPath: #keyPath(CALayer.cornerRadius))
+    public static let position = AnimatorKeyPath(keyPath: #keyPath(CALayer.position))
+    public static let backgroundColor = AnimatorKeyPath(keyPath: #keyPath(CALayer.backgroundColor))
+    public static let transform = AnimatorKeyPath(keyPath: #keyPath(CALayer.transform))
 }
 
-extension AnimationKeyPaths {
-    public static let positionX = AnimatorKeyPath<CGFloat>(keyPath: "\(#keyPath(CALayer.position)).x")
-    public static let positionY = AnimatorKeyPath<CGFloat>(keyPath: "\(#keyPath(CALayer.position)).y")
+extension AnimatorKeyPath {
+    public static let positionX = AnimatorKeyPath(keyPath: "\(#keyPath(CALayer.position)).x")
+    public static let positionY = AnimatorKeyPath(keyPath: "\(#keyPath(CALayer.position)).y")
 }
 
-extension AnimationKeyPaths {
-    public static let boundsOrigin     = AnimatorKeyPath<CGPoint>(keyPath: "\(#keyPath(CALayer.bounds)).origin")
-    public static let boundsOriginX    = AnimatorKeyPath<CGFloat>(keyPath: "\(#keyPath(CALayer.bounds)).origin.x")
-    public static let boundsOriginY    = AnimatorKeyPath<CGFloat>(keyPath: "\(#keyPath(CALayer.bounds)).origin.y")
-    public static let boundsSize       = AnimatorKeyPath<CGSize>(keyPath: "\(#keyPath(CALayer.bounds)).size")
-    public static let boundsSizeWidth  = AnimatorKeyPath<CGFloat>(keyPath: "\(#keyPath(CALayer.bounds)).size.width")
-    public static let boundsSizeHeight = AnimatorKeyPath<CGFloat>(keyPath: "\(#keyPath(CALayer.bounds)).size.height")
+extension AnimatorKeyPath {
+    public static let boundsOrigin     = AnimatorKeyPath(keyPath: "\(#keyPath(CALayer.bounds)).origin")
+    public static let boundsOriginX    = AnimatorKeyPath(keyPath: "\(#keyPath(CALayer.bounds)).origin.x")
+    public static let boundsOriginY    = AnimatorKeyPath(keyPath: "\(#keyPath(CALayer.bounds)).origin.y")
+    public static let boundsSize       = AnimatorKeyPath(keyPath: "\(#keyPath(CALayer.bounds)).size")
+    public static let boundsSizeWidth  = AnimatorKeyPath(keyPath: "\(#keyPath(CALayer.bounds)).size.width")
+    public static let boundsSizeHeight = AnimatorKeyPath(keyPath: "\(#keyPath(CALayer.bounds)).size.height")
 }
 
-extension AnimationKeyPaths {
-    public static let scale  = AnimatorKeyPath<CGFloat>(keyPath: "\(#keyPath(CALayer.transform)).scale")
-    public static let scaleX = AnimatorKeyPath<CGFloat>(keyPath: "\(#keyPath(CALayer.transform)).scale.x")
-    public static let scaleY = AnimatorKeyPath<CGFloat>(keyPath: "\(#keyPath(CALayer.transform)).scale.y")
-    public static let scaleZ = AnimatorKeyPath<CGFloat>(keyPath: "\(#keyPath(CALayer.transform)).scale.z")
+extension AnimatorKeyPath {
+    public static let scale  = AnimatorKeyPath(keyPath: "\(#keyPath(CALayer.transform)).scale")
+    public static let scaleX = AnimatorKeyPath(keyPath: "\(#keyPath(CALayer.transform)).scale.x")
+    public static let scaleY = AnimatorKeyPath(keyPath: "\(#keyPath(CALayer.transform)).scale.y")
+    public static let scaleZ = AnimatorKeyPath(keyPath: "\(#keyPath(CALayer.transform)).scale.z")
 }
